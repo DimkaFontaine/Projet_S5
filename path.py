00006900cbd58fa2fd46b581773a299d3c8bde65
Mascool <antoine.mascolo3428@gmail.com>
2021-03-15 19:08:20 -0400

    Angle in path

--- path.py
+++ path.py
@@ -41,30 +41,33 @@ def straightPath(name, scale_x = 0.009,scale_y = 0.009, loc_x =0, loc_y =0):
     path =C.active_object
     path.scale = (scale_x,scale_y, 0)
     path.location = (loc_x, loc_y,0)
-    
     return path
-    
     
     #direction L = left
     #          R = right
-def turnPath(name, radius, direction = 'L', loc_x =0, loc_y =0):
+def turnPath(name, radius, angle, direction = 'L',loc_x =0, loc_y =0):
+    if direction == 'L':
+        direction_factor =-1
+        angle_offset= 270
+    else:
+        angle_offset= 90
+        direction_factor = 1
+    
+    #Create path plane
     O.mesh.primitive_plane_add()  
     path = C.active_object
     C.active_object.name = name
     path.scale = (radius, radius, 1.0)
     path.location = (0.0, radius, 0.0)
     
+    #Create a cylinder to cut the plane inner radius
     bpy.ops.mesh.primitive_cylinder_add(vertices = 128) 
     C.active_object.name = "Hole maker" 
     hole_maker = C.active_object
     hole_maker.scale = ((radius - 0.009), (radius - 0.009), 1.0) 
     
-    if direction == 'L':
-        direction_location = radius * -1
-    else:
-        direction_location = radius
-    
-    hole_maker.location = (direction_location , 0.0, 0.0)
+    #Rescle the cylinder to cut the outer radius 
+    hole_maker.location = ((direction_factor*radius) , 0.0, 0.0)
     
     makeHole(path, hole_maker)
     
@@ -73,9 +76,39 @@ def turnPath(name, radius, direction = 'L', loc_x =0, loc_y =0):
     
     makeIntersect(path, hole_maker)
     
+    #Delete cylinder
     O.object.select_all(action='DESELECT')
     path.select_set(False)
     D.objects['Hole maker'].select_set(True)
+    O.object.delete()
+    
+    #Create cube to cut the path at the right angle
+    bpy.ops.mesh.primitive_cube_add() 
+    C.active_object.name = "Angle maker" 
+    angle_maker = C.active_object
+    angle_maker.scale = ((radius+1), 2*radius, 1.0)
+    angle_maker.location = ((direction_factor*radius), 0 , 0)
+    
+    bpy.ops.mesh.primitive_cube_add() 
+    C.active_object.name = "Hole maker2" 
+    hole_maker2 = C.active_object
+    hole_maker2.scale = (radius, (2*radius+0.1), 1.1)
+    
+    makeHole(angle_maker, hole_maker2)
+    
+    #Delete hole_maker2
+    O.object.select_all(action='DESELECT')
+    path.select_set(False)
+    D.objects['Hole maker2'].select_set(True)
+    O.object.delete()
+
+    #Rotate and cut
+    angle_maker.rotation_euler = (0,0,math.radians(angle_offset-(angle*direction_factor)))
+    makeHole(path, angle_maker)
+    
+    O.object.select_all(action='DESELECT')
+    path.select_set(False)
+    D.objects['Angle maker'].select_set(True)
     O.object.delete() 
     
     return path
@@ -107,18 +140,18 @@ def straightLineForwardWithObstacleInMiddleTest():
     lineToCarSupport.location = (0,1,0)
 
 def leftCurveTest():
-    turnPath("leftTest", 0.12, direction = 'L')
+    turnPath("leftTest", 0.12, 90, direction = 'L')
     
 
 def rightCurveTest():
-    turnPath("rightTest", 0.5, direction = 'R')
+    turnPath("rightTest", 0.5, 90, direction = 'R')
 
 def tightLeftCurveTest():
-    turnPath("leftTightTest", 0.12, direction = 'L')
+    turnPath("leftTightTest", 0.12, 90, direction = 'L')
     
    
 def tightRightCurveTest():
-    turnPath("rightTightTest", 0.12, direction = 'R')
+    turnPath("rightTightTest", 0.12, 90, direction = 'R')
 print("Reset") 
 
 clearMesh()      # destroy all mesh object && reset animation too the start
@@ -127,6 +160,6 @@ os.system("cls") # clean console
 print() 
 print("Start") 
 
-rightCurveTest()
+turnPath("test", 0.35, 90,'L')
 
 print("end")
